ad850dc163609854f7aeb1daa7d5a6ebe59feba2
Ben Lerchin <ben@lerchin.com>
2016-05-15 14:57:49 -0700

    yikes

--- imports.php
+++ imports.php
@@ -25,5 +25,5 @@
 		<?php else: ?>
 			<script type="text/javascript" 
 			src="<?php echo get_stylesheet_directory_uri() . "/dist/index.js" ?>"></script>
-			<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri() . "/dist/index.js" ?>"></link>
+			<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri() . "/dist/index.css" ?>"></link>
 		<?php endif; ?>
