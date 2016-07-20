c37784c933a0cc0db5324c59ffc8df4839c6364d
Kieran Startup <info@kieranstartup.co.uk>
2016-07-20 17:40:36 +0100

    turned off the email signup after the of supply chains section and moved it underneath the projects section

--- page-bfamfaphd.php
+++ page-bfamfaphd.php
@@ -18,14 +18,25 @@
   </article>
   <?php get_template_part('articles/topics-list')?>
   <?php get_template_part('articles/of-supply-chains-interstitial')?>
-  <article <?php post_class('item item-full') ?> id="topic-form">
+
+<!--  <article /* <?php post_class('item item-full') ?> */ id="topic-form">
+    <div class="article-body-wrapper">
+      <div class="article-body">
+        <?php /* echo bfamfaphd_mailchimp_form(); */ ?>
+      </div>
+    </div>
+  </article> -->
+
+  <?php get_template_part('articles/projects-list')?>
+
+<article <?php post_class('item item-full') ?> id="topic-form">
     <div class="article-body-wrapper">
       <div class="article-body">
         <?php echo bfamfaphd_mailchimp_form(); ?>
       </div>
     </div>
   </article>
-  <?php get_template_part('articles/projects-list')?>
+
 <?php endwhile; else : ?>
   <p><?php _e('Sorry, nothing found at this address.'); ?></p>
 <?php endif; ?>
