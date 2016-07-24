d0800f40fd897a44e92c221a70959488e0db4eb8
Ben Lerchin <ben@lerchin.com>
2016-07-24 00:59:57 -0700

    dist

--- imports.php
+++ imports.php
@@ -1,6 +1,6 @@
 		<?php $dev = preg_match('/local$/', $_SERVER['HTTP_HOST']) ?>
 
-		<?php 
+		<?php
 			$cardsJson = get_posts_json('card');
 			$supplyChainsJson = get_posts_json('supply_chain');
 		?>
@@ -9,14 +9,14 @@
 			window.g = {};
 			window.g.supplyChains = <?php echo $supplyChainsJson; ?>;
 			window.g.cards 				= <?php echo $cardsJson; ?>;
-			
+
 		</script>
 
-		<?php /* if($dev): */ ?>
+		<?php if($dev): ?>
 			<script type="text/javascript" src="//localhost:3000/index.js"></script>
 			<link rel="stylesheet" type="text/css" href="//localhost:3000/index.css"></link>
-		<?php /* else: */ ?>
-			<!-- <script type="text/javascript" 
+		<?php else: ?>
+			<script type="text/javascript"
 			src="<?php echo get_stylesheet_directory_uri() . "/dist/index.js" ?>"></script>
-			<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri() . "/dist/index.css" ?>"></link>-->
-		<?php /* endif; */ ?>
+			<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri() . "/dist/index.css" ?>"></link>
+		<?php endif; ?>
