b92eca1c981c56e980e40c13624d1a609b8e0cab
Ben Lerchin <ben@lerchin.com>
2016-08-04 23:49:29 -0700

    add file

---
+++ page-supply-chains-list.php
@@ -0,0 +1,19 @@
+<?php
+global $SITE_SECTION;
+$SITE_SECTION = "supply-chains";
+global $SHOW_CIRCLE;
+$SHOW_CIRCLE = false;
+?>
+<?php get_header(); ?>
+<?php $chains = new WP_Query(array(
+  'post_type' => 'supply_chain',
+  'orderby' => 'menu_order',
+  'order' => 'ASC',
+  'posts_per_page' => '12'
+)); ?>
+<?php if ($chains->have_posts() ) : while ( $chains->have_posts() ) : $chains->the_post(); ?>
+  <?php get_template_part('articles/article', 'supply_chain'); ?>
+<?php endwhile; ?>
+<?php endif; ?>
+<?php echo do_shortcode('[bfamfaphd_mailchimp_form]'); ?>
+<?php get_footer(); ?>
