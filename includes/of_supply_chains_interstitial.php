6b889b3d8c004a22b4bcd1745ba1039f2d3f7d27
Ben Lerchin <ben@lerchin.com>
2016-08-08 09:33:36 -0700

    make homepage link point to /cards

--- includes/of_supply_chains_interstitial.php
+++ includes/of_supply_chains_interstitial.php
@@ -7,7 +7,7 @@ function bfamfaphd_interstitial_module(){
 	      $out .= '<h3 class="lead">';
 	      $out .= 'Of Supply Chains is a teaching tool that traces the life cycle of projects — the ways materials are sourced, how the labor for producing a project is organized, how tools are accessed, how a project is supported, licensed, narrated, encountered, acquired, and how it finally departs, ready for another life cycle. Of Supply Chains provides a vocabulary and a set of practices to think through economies of solidarity in the arts.';
 	      $out .= '</h3>';
-	      $out .= '<h3><a href="/supply-chains">Visit Of Supply Chains →</a></h3>';
+	      $out .= '<h3><a href="/cards">Visit Of Supply Chains →</a></h3>';
 	$out .= '</div>';
 	return $out;
 }
